a06fa4ccb116f5f41cd9fce988d8809d53bbc25c
Alex Vo <votanlean@gmail.com>
2022-11-23 12:23:49 +0700

    Support option arguments

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const fs = require("node:fs");
 const { parse } = require("csv-parse");
+const yargs = require("yargs");
+const argv = yargs
+  .command("report", "Show simple report from the transactions file")
+  .option("token", {
+    alias: "t",
+    description: "Token Symbol",
+    type: "string",
+  })
+  .option("date", {
+    alias: "d",
+    description: "Date in format YYYYMMDD",
+    type: "string",
+  })
+  .help()
+  .alias("help", "h").argv;
+
+if (argv.token) {
+  console.log("You are find with token: ", argv.token);
+}
+if (argv.date) {
+  console.log("You are find on date: ", new Date(argv.date));
+}
+
+console.log(argv);
+
 const result = {
   BTC: 0,
   ETH: 0,
