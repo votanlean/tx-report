ac918e890d8bae363575d89f8a8eec07ae9e0ed1
Alex Vo <votanlean@gmail.com>
2022-11-23 15:46:10 +0700

    Filter by token Support custom filepath Ignore the source data folder, just keep the sample data

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # tx-report
+
+# Instruction
+
+Put source file into the data folder (e.g: `./data/transactions.csv`)
+
+Clone the .env.example to .env and change the `FILE_PATH` to match the source file
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+require("dotenv").config();
 const fs = require("node:fs");
 const { parse } = require("csv-parse");
 const yargs = require("yargs");
+
+const supportToken = ["BTC", "ETH", "XRP"];
+const filePath = process.env.FILE_PATH || "./data/sample.csv";
+const result = supportToken.reduce((res, key) => ((res[key] = 0), res), {});
 const argv = yargs
   .command("report", "Show simple report from the transactions file")
   .option("token", {
@@ -16,12 +21,27 @@ const argv = yargs
   .help()
   .alias("help", "h").argv;
 
-const result = {
-  BTC: 0,
-  ETH: 0,
-  XRP: 0,
-};
-fs.createReadStream("./simple_tx.csv")
+if (argv.token) {
+  if (supportToken.includes(argv.token)) {
+    console.log("Filtered by token: ", argv.token);
+  } else {
+    console.error(`Filtered token ${argv.token} is not support`);
+    process.exit(1);
+  }
+}
+
+if (argv.date) {
+  const filteredDate = new Date(argv.date);
+  if (filteredDate instanceof Date && !isNaN(filteredDate)) {
+    console.log("Filtered by date: ", argv.date);
+  } else {
+    console.error(
+      `Invalid filter date: ${argv.date}, the format is YYYY-MM-DD`
+    );
+  }
+}
+
+fs.createReadStream(filePath)
   .pipe(parse({ delimiter: ",", from_line: 2 }))
   .on("data", function (row) {
     // timestamp, action, symbol, amount
@@ -29,6 +49,12 @@ fs.createReadStream("./simple_tx.csv")
     const action = row[1];
     const symbol = row[2];
     const amount = parseFloat(row[3]);
+    if (argv.token) {
+      if (symbol !== argv.token) {
+        return;
+      }
+    }
+
     if (argv.date) {
       const dateBeginingTimestamp = new Date(argv.date).valueOf() / 1000;
       if (
@@ -49,13 +75,6 @@ fs.createReadStream("./simple_tx.csv")
     }
   })
   .on("end", function () {
-    if (argv.date) {
-      console.log("Filtered by date: ", new Date(argv.date));
-    }
-
-    if (argv.token) {
-      console.log("Filtered by token: ", argv.token);
-    }
     console.log("Result", result);
   })
   .on("error", function (error) {
